f55485b77df2f593fb5be5de6cdd3e09edf95eb5
Daria Rezvin <109664398+dariarez@users.noreply.github.com>
2023-03-16 00:14:39 +0200

    Update p.py

--- p.py
+++ p.py
@@ -2,23 +2,23 @@ import requests
 import json
 
 
-a = int(input('введіть id користувача: '))
-b = int(input('введіть баланс: '))
+a = int(input('enter user`s id: '))
+b = int(input('enter balance: '))
 
 def balance(a):
-    url2 = "https://coderlog.top/api/goit/?key=5b15bdfa142761a1c65f50e046b6f7f5&method=getdata&user={}".format(a)
+    url2 = "your_key&method=getdata&user={}".format(a)
     res = requests.get(url2)
     print(res.text)
 
 def new(a,b):
-    check_user = "https://coderlog.top/api/goit/?key=5b15bdfa142761a1c65f50e046b6f7f5&method=getdata&user={}".format(a)
+    check_user = "your_key&method=getdata&user={}".format(a)
     res_check_user = requests.get(check_user)
     data = res_check_user.json()
 
     if data[0]['id'] == None:
         print('not found')
     else:
-        url = "https://coderlog.top/api/goit/?key=5b15bdfa142761a1c65f50e046b6f7f5&method=setbalance&user={}&balance={}".format(a,b)
+        url = "your_key&method=setbalance&user={}&balance={}".format(a,b)
         res = requests.get(url)
 
         balance(a)
